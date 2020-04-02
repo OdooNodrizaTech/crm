1b2f2040e74fef259eff8cd26a0cdad13cd28168
Informatica Arelux <informatica@arelux.com>
2020-04-02 14:14:22 +0200

    crm_arelux

---
+++ crm_arelux/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from . import crm_activity_objective
+from . import crm_activity_log
+from . import crm_lead
+from . import mail_message
+from . import mail_message_little
+from . import res_partner
---
+++ crm_arelux/report/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from . import crm_activity_report
+from . import crm_lead_mail_message_report
+from . import crm_lead_report
+from . import res_partner_account_invoice_report
+from . import res_partner_mail_message_report
+from . import res_partner_report
+from . import sale_order_account_invoice_report
+from . import crm_lead_account_invoice_report#Fix final
