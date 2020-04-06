adecb1180757c9ba1ca00b6a2893caf52c7af15a
Informatica Arelux <informatica@arelux.com>
2020-04-06 07:57:32 +0200

    separacion

--- crm_claim/models/sale_order.py
+++ crm_claim/models/sale_order.py
@@ -21,3 +21,19 @@ class SaleOrder(models.Model):
     def change_claim_id(self):
         if self.claim_id.id>0 and self.partner_id.id==0:
             self.partner_id = self.claim_id.partner_id
+            
+    @api.model
+    def create(self, values): 
+        allow_create = True
+        #operations
+        if 'claim' in values:
+            if 'claim_id' in values:
+                if values['claim']==True:
+                    if values['claim_id']==False:
+                        allow_create = False
+                        raise Warning("Es necesario definir una reclamacion")
+                    else:
+                        values['name'] = self.env['ir.sequence'].next_by_code('sale.order.claim') or 'New Claim'                    
+        #allow_create
+        if allow_create==True:
+            return super(SaleOrder, self).create(values)                        
