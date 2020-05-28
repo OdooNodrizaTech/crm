3581af1fa78e0fb7fac3eafe0ef89bea9016bc7c
Informatica Arelux <informatica@arelux.com>
2020-05-28 11:37:30 +0200

    https://grupoarelux.atlassian.net/browse/ODOOARELUX-795

--- crm_claim/models/crm_claim.py
+++ crm_claim/models/crm_claim.py
@@ -147,8 +147,7 @@ class CrmClaim(models.Model):
             elif model_ref_name=='sale.order' or model_ref_name=='purchase.order' or model_ref_name=='account.invoice':
                 model_ref_search_ids = self.env[model_ref_name].search([('id', '=', model_ref_id)])[0]
                 values['partner_id'] = model_ref_search_ids.partner_id.id
-                values['carrier_id'] = model_ref_search_ids.carrier_id.id            
-            
+        #return
         return super(CrmClaim, self).create(values)
 
     @api.multi
